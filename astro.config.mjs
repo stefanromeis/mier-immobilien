8adf301dc9e15e1934f39aaba005c55ee88ba3ab
Stefan Romeis <s.romeis@gmail.com>
2026-02-01 00:41:47 +0100

    Update Astro configuration with site URL and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://stefanromeis.github.io/mier-immobilien/',
+  base: '/mier-immobilien/',
   integrations: [react(), tailwind()],
   output: 'static',
 });
